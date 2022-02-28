2f9d24a18e944bd2c36affbde313a273260f60f4
nguyentranthin <100121429+nguyentranthin@users.noreply.github.com>
2022-02-28 15:06:26 +0700

    Add files via upload

---
+++ baitap.py
@@ -0,0 +1,11 @@
+Python 2.7.10 (default, May 23 2015, 09:40:32) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("Helo,word")
+Helo,word
+>>> print("ho ten: Nguyen Tran Thin")
+ho ten: Nguyen Tran Thin
+>>> print("Msv: 205752021610055")
+Msv: 205752021610055
+>>> print("Nganh Dieu Khien Va Tu Dong Hoa")
+Nganh Dieu Khien Va Tu Dong Hoa
+>>> 
